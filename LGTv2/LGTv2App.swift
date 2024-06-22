f9243ff5fd96b687990b9aabe483e34c5d005a3a
joe <joescholtz@hotmail.com>
2024-06-22 14:18:44 -0500

    Initial Commit

---
+++ LGTv2/LGTv2App.swift
@@ -0,0 +1,17 @@
+//
+//  LGTv2App.swift
+//  LGTv2
+//
+//  Created by Joseph Scholtz on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LGTv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
