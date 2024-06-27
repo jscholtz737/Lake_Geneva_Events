7b11f7a87b6dd01296eefbcb6a1c6fbd12b904bc
joe <joescholtz@hotmail.com>
2024-06-27 11:53:08 -0400

    a working connection to firestore

--- LGTv2/LGTv2App.swift
+++ LGTv2/LGTv2App.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import FirebaseCore
+import Firebase
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -19,8 +21,25 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct LGTv2App: App {
     // register app delegate for Firebase setup
-      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
+      //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        let events = db.collection("events")
+        let document = events.document("Pq4HkmIp45Q5A5yLezxu")
+        document.getDocument { docSnapshot, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let docSnapshot = docSnapshot {
+                print(docSnapshot.data() ?? "nil")
+            } else {
+                //no data returned
+            }
+            
+        }
+    }
+        
       var body: some Scene {
         WindowGroup {
           NavigationView {
