22eee9bbcfc15870c2fe9ed8164f552b8016dd51
joe <joescholtz@hotmail.com>
2024-06-27 12:13:45 -0400

    cleans up appdelagate error

--- LGTv2/LGTv2App.swift
+++ LGTv2/LGTv2App.swift
@@ -14,32 +14,27 @@ class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
-    return true
+      let db = Firestore.firestore()
+      let events = db.collection("events")
+      let document = events.document("Pq4HkmIp45Q5A5yLezxu")
+      document.getDocument { docSnapshot, error in
+          if let error = error {
+              print(error.localizedDescription)
+          } else if let docSnapshot = docSnapshot {
+              print(docSnapshot.data() ?? "nil")
+          } else {
+              //no data returned
+          }
+      }
+      return true
   }
 }
 
 @main
 struct LGTv2App: App {
     // register app delegate for Firebase setup
-      //@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    init() {
-        FirebaseApp.configure()
-        let db = Firestore.firestore()
-        let events = db.collection("events")
-        let document = events.document("Pq4HkmIp45Q5A5yLezxu")
-        document.getDocument { docSnapshot, error in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let docSnapshot = docSnapshot {
-                print(docSnapshot.data() ?? "nil")
-            } else {
-                //no data returned
-            }
-            
-        }
-    }
-        
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+ 
       var body: some Scene {
         WindowGroup {
           NavigationView {
